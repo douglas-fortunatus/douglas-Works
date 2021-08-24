d057fdade44b72779852cfb663071b40fc879fce
Douglas <52981261+douglas-fortunatus@users.noreply.github.com>
2021-08-24 16:33:42 +0300

    bootstrap remove

--- pages/_document.js
+++ pages/_document.js
@@ -24,7 +24,6 @@ export default class MyDocument extends Document{
                     <meta name="keywords" content="Douglas Fortunatus, Doku, DCODE, DxCODE" />
 
                     <link rel="stylesheet" href="/css/normalize.css" />
-                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
                 </Head>
 
                 <body>
